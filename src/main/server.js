3b1e2bc5f6648556186e944637f3dfebb08f7aad
Benny Neugebauer <bn@bennyn.de>
2017-07-13 17:31:53 +0200

    feat: Serving static files

--- src/main/server.js
+++ src/main/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const WebSocket = require('ws');
 
 const PORT = parseInt(process.env.PORT, 10) || 8087;
+const PUBLIC_DIR = path.join(__dirname, 'public');
 
 const app = express();
+app.use(express.static(PUBLIC_DIR));
 const httpServer = http.createServer(app);
-const wsServer = new WebSocket.Server({server: httpServer});
 
+const wsServer = new WebSocket.Server({server: httpServer});
 wsServer.on('listening', () => console.log(`WebSocket server listening on port "${PORT}"...`));
 wsServer.on('message', () => console.log(`Message received: ${message}`));
 wsServer.on('connection', (ws) => {
